5ef515b8b474fd1fbbb49ae4007d450a1756bd58
Lucas Renó <lucasreno9@gmail.com>
2022-06-02 20:37:14 -0300

    Ajustado home, com descrição imagem e preço

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Categoria } from '../interface/categoria';
+import { Produto } from '../interface/produto';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,13 @@ export class HomePage {
     this.http.get<Categoria[]>(this.URL_BASE+'/categorias').subscribe(
       resposta => {
         this.dados = resposta;
+        this.dados.forEach(dado => {
+          this.http.get<Produto[]>(this.URL_BASE+'/produtos/'+dado.idCategoria).subscribe(
+            resp => {
+              dado.produtos = resp;
+            }
+          );
+        });
       }
     );
   }
