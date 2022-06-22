cb8b3cdbd29aa9c5d5f57808a1a1fb514241ba73
Lucas Renó <lucasreno9@gmail.com>
2022-06-21 22:26:51 -0300

    feat: Adicionado formulario na tela de carrinho

--- src/app/carrinho/carrinho.page.ts
+++ src/app/carrinho/carrinho.page.ts
@@ -10,6 +10,13 @@ import { Carrinho } from '../interface/carrinho';
 export class CarrinhoPage implements OnInit {
   carrinho: Carrinho[] = [];
   totalCarrinho: number = 0;
+  nome = null;
+  telefone = null;
+  entrega = null;
+  endereco = null;
+  pagamento = null;
+  mostrarEndereco = false;
+
   constructor(
     private storage: Storage
   ) { }
@@ -18,7 +25,7 @@ export class CarrinhoPage implements OnInit {
     this.iniciarBanco();
   }
 
-  async iniciarBanco(){
+  async iniciarBanco() {
     await this.storage.create();
     this.carrinho = await this.storage.get('carrinho') ?? [];
     this.totalCarrinho = 0;
@@ -30,8 +37,8 @@ export class CarrinhoPage implements OnInit {
   ngOnInit() {
   }
 
-  remover(indice){
-    this.carrinho.splice(indice,1);
+  remover(indice) {
+    this.carrinho.splice(indice, 1);
     this.storage.set('carrinho', this.carrinho);
     this.totalCarrinho = 0;
     this.carrinho.forEach(c => {
@@ -39,4 +46,30 @@ export class CarrinhoPage implements OnInit {
     });
   }
 
+  verificar() {
+    if (this.entrega == 'delivery')
+      this.mostrarEndereco = true;
+    else
+      this.mostrarEndereco = false;
+  }
+
+  enviarPedido() {
+    let texto = '';
+    texto += 'Nome: ' + this.nome + '\n';
+    texto += 'Telefone: ' + this.telefone + '\n';
+    texto += 'Tipo de entrega: ' + this.entrega + '\n';
+    if (this.entrega == 'delivery')
+      texto += 'Endereço: ' + this.endereco + '\n';
+    texto += 'Forma de pagamento: ' + this.pagamento + '\n';
+    texto += '\nPedido:';
+    this.carrinho.forEach(c => {
+      texto += c.quantidade + ' x '+ c.produto.nome + '\n';
+      texto += '          R$ ' + (c.quantidade * c.produto.valor).toFixed(2) + '\n';
+    });
+    texto += '\nTotal:';
+    texto += '          R$ ' + this.totalCarrinho.toFixed(2) + '\n';
+    console.log(texto);
+    const url = 'https://api.whatsapp.com/send?phone=5515996580305&text=' + encodeURI(texto);
+    window.open(url, '_blank').focus();
+  }
 }
